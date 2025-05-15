dce78c6f6ad877edc3d1542ca432ea1539b4f1f2
alvinabner <alvinabnert@gmail.com>
2025-05-15 13:23:49 +0700

    Fix on index_employee.go

--- controller/index_employee.go
+++ controller/index_employee.go
@@ -1,13 +1,49 @@
 package controller
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
 	"path/filepath"
 )
 
-func NewIndexEmployee() func(w http.ResponseWriter, r *http.Request) {
+type Employee struct {
+	Id      string
+	Name    string
+	NPWP    string
+	Address string
+}
+
+func NewIndexEmployee(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT id, name, npwp, address FROM employee")
+
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var employees []Employee
+		for rows.Next() {
+			var employee Employee
+
+			rows.Scan(
+				&employee.Id,
+				&employee.Name,
+				&employee.NPWP,
+				&employee.Address,
+			)
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			employees = append(employees, employee)
+		}
+
 		fp := filepath.Join("views", "index.html")
 		tmpl, err := template.ParseFiles(fp)
 
