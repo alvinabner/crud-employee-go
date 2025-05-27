8c3a5b34b40fd95643b10a1e6cff413cab713a83
alvinabner <alvinabnert@gmail.com>
2025-05-27 23:54:01 +0700

    add delete controller

--- routes/routes.go
+++ routes/routes.go
@@ -12,4 +12,5 @@ func MapRoutes(server *http.ServeMux, db *sql.DB) {
 	server.HandleFunc("/employee", controller.NewIndexEmployee(db))
 	server.HandleFunc("/employee/create", controller.NewCreateEmployeeController(db))
 	server.HandleFunc("/employee/update", controller.NewUpdateEmployeeController(db))
+	server.HandleFunc("/employee/delete", controller.NewDeleteEmployeeController(db))
 }
